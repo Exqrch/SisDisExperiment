be1c4ff77a3fa21a0d1f745138f620014f56789a
Zsaschz <fathanmuthahhari@gmail.com>
2023-05-23 03:01:10 +0700

    implement stop and restore vsr

--- test_vsr.py
+++ test_vsr.py
@@ -1,44 +1,43 @@
-from vsr import Client, Replica
+from vsr_2 import Client, Replica
 from network import Network
 import threading
 import time
+import logging
+import multiprocessing
 
 def main():
+
     timer = time.monotonic_ns()
 
     ip = "127.0.0.1"
     node_ids = [0, 1, 2, 3, 4]
     node_addresses = [(ip, port) for port in [5001, 5002, 5003, 5004, 5005]]
-    clients = {5000: (ip, 5000)}
+    client_addr =  ("127.0.0.1", 5000)
 
     network = Network(node_ids, node_addresses)
 
     f = 1
     quorumsize = f + 1
+    
+    for i in node_ids:
+        run_process = multiprocessing.Process(target=start_replica, args=(network, i, node_addresses[i], quorumsize, 10))
+        run_process.name = f"run_process_replica_{i}"
+        run_process.start()
 
-    replica_0 = Replica(network, 0, node_addresses[0], quorumsize, 30, clients)
-    replica_1 = Replica(network, 1, node_addresses[1], quorumsize, 30, clients)
-    replica_2 = Replica(network, 2, node_addresses[2], quorumsize, 30, clients)
-    replica_3 = Replica(network, 3, node_addresses[3], quorumsize, 30, clients)
-    replica_4 = Replica(network, 4, node_addresses[4], quorumsize, 30, clients)
+    print("All replicas are running", time.monotonic_ns() - timer)
 
-    replicas = [replica_0, replica_1, replica_2, replica_3, replica_4]
-
-    for replica in replicas:
-        run_thread = threading.Thread(target=replica.run)
-        run_thread.name = f"run_thread_{replica.node_id}"
-        run_thread.start()
-
-    print("All replicas are listening", time.monotonic_ns() - timer)
-
-    client = Client(node_addresses, 30, ip, 5000)
-    listen_thread = threading.Thread(target=client.listen)
-    listen_thread.name = f"client_listen_thread"
-    listen_thread.start()
-
-    client.run()
+    client_process = multiprocessing.Process(target=start_client, args=(node_addresses, 30, client_addr))
+    client_process.name = "client_process"
+    client_process.run()
     print("TIME TAKEN", time.monotonic_ns() - timer)
-
+    
+def start_replica(network, id, address, quorumsize, timeout):
+    replica = Replica(network, id, address, quorumsize, timeout, ("127.0.0.1", 5000))
+    replica.run()
+ 
+def start_client(node_addresses, timeout, client_addr):
+    client = Client(node_addresses, timeout, client_addr)
+    client.run()
 
 if __name__ == "__main__":
     main()
